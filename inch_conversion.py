17b506e0b72e49dd365bdaf6411c35547cd5affe
aashi1408 <aashiv2525@gmail.com>
2025-11-19 21:29:53 +0530

    Add files via upload
    Write a program that converts and prints user given measurement in inches into
    1. foot(12 inches)
    2. yard(36 inches)
    3. centimetres(2.54 inches)
    4. meter(39.37 inches)

---
+++ inch_conversion.py
@@ -0,0 +1,7 @@
+#inch_conversion
+
+inches = int(input())
+print("In Foot/Feet : ",inches/12)
+print("In Yard : ",inches/36)
+print("In Centimeters : ",2.54*inches)
+print("In meters : ",inches/39.37)
