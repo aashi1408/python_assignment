9763b4ace25b90f8776a44338f0975fcdc21a435
aashi1408 <aashiv2525@gmail.com>
2025-11-19 21:28:21 +0530

    Add files via upload
    John is trying to solve the arithmetic series(AP). He wants to find two things in the series
    1. He wants to find nth terms in the series
    2. He wants to find the sum up to the nth term.

---
+++ john_and_his_series.py
@@ -0,0 +1,13 @@
+#John_and_his_series
+#first we'll input : a(the first term of the series)
+#d(the common difference) and n(upto where we have to find the series)
+a = int(input())
+d = int(input())
+n = int(input())
+b = a+(n-1)*d#the AP eqn
+
+sum = 0
+for g in range(a,b,d):
+    sum+=g
+    print(g,end=' ')
+print("Sum upto nth term = ",sum)
